fdc520842218eb00366ef2bb827bd886ed79e7c9
yongqi <yongqi020203@gmail.com>
2021-09-15 00:43:48 +0800

    .

--- Assignment sem1/yong qi/mp_formsubmit.js
+++ Assignment sem1/yong qi/mp_formsubmit.js
@@ -2,8 +2,8 @@ window.onload=setForm;
 
 function setForm(){
     document.form[0].onsubmit=function(){
-        if(this.checkValidity())
-        alert("Successful entered the details.Please enjoy on your purchasing");
+        if(this.checkValidity()) alert("Successful login your profile. Please enjoy on your purchasing");
+        document.getElementById("form").reset();
         return true;
     }
 }
