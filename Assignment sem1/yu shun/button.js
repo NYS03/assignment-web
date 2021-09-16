92e73004d7c73b3e455f89a0fc43bff6e710d575
Ng Yu Shun <ngyushun@gmail.com>
2021-09-16 20:23:01 +0800

    yes

---
+++ Assignment sem1/yu shun/button.js
@@ -0,0 +1,156 @@
+function incrementButton1() {
+    var value = parseInt(document.getElementById('cart-quantity1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity1').value = value;
+    console.log(value);
+}
+
+function decrementButton1() {
+    var value = parseInt(document.getElementById('cart-quantity1').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity1').value = value;
+    console.log(value);
+}
+
+function incrementButton2() {
+    var value = parseInt(document.getElementById('cart-quantity2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity2').value = value;
+    console.log(value);
+}
+
+function decrementButton2() {
+    var value = parseInt(document.getElementById('cart-quantity2').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity2').value = value;
+    console.log(value);
+}
+
+function incrementButton3() {
+    var value = parseInt(document.getElementById('cart-quantity3').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity3').value = value;
+    console.log(value);
+}
+
+function decrementButton3() {
+    var value = parseInt(document.getElementById('cart-quantity4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity3').value = value;
+    console.log(value);
+}
+
+function incrementButton4() {
+    var value = parseInt(document.getElementById('cart-quantity4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity4').value = value;
+    console.log(value);
+}
+
+function decrementButton4() {
+    var value = parseInt(document.getElementById('cart-quantity4').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity4').value = value;
+    console.log(value);
+}
+
+function incrementButton5() {
+    var value = parseInt(document.getElementById('cart-quantity5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity5').value = value;
+    console.log(value);
+}
+
+function decrementButton5() {
+    var value = parseInt(document.getElementById('cart-quantity5').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity5').value = value;
+    console.log(value);
+}
+
+function incrementButton6() {
+    var value = parseInt(document.getElementById('cart-quantity6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity6').value = value;
+    console.log(value);
+}
+
+function decrementButton6() {
+    var value = parseInt(document.getElementById('cart-quantity6').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity6').value = value;
+    console.log(value);
+}
+
+function incrementButton7() {
+    var value = parseInt(document.getElementById('cart-quantity7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity7').value = value;
+    console.log(value);
+}
+
+function decrementButton7() {
+    var value = parseInt(document.getElementById('cart-quantity7').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity7').value = value;
+    console.log(value);
+}
+
+function incrementButton8() {
+    var value = parseInt(document.getElementById('cart-quantity8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity8').value = value;
+    console.log(value);
+}
+
+function decrementButton8() {
+    var value = parseInt(document.getElementById('cart-quantity8').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity8').value = value;
+    console.log(value);
+}
+
+function incrementButton9() {
+    var value = parseInt(document.getElementById('cart-quantity9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('cart-quantity9').value = value;
+    console.log(value);
+}
+
+function decrementButton9() {
+    var value = parseInt(document.getElementById('cart-quantity9').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('cart-quantity9').value = value;
+    console.log(value);
+}
+
+function productpagetotal() {
+    var totalPrice = document.getElementsByClassName("cart-price");
+    var quantity = document.getElementById('cart-quantity1');
+    var price = document.getElementsByClassName("price1");
+
+    var sum = quantity * price;
+    sum = Math.round(sum * 100) / 100;
+    totalPrice.innerHTML = 'RM' + sum;
+
+}
+
+
